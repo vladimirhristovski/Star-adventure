49f3735097ceef445dba94c70dea234edea936d6
vladimirhristovski <vladimir13hristovski@gmail.com>
2024-06-29 08:38:16 +0200

    first progress

--- Star Adventure/Star Adventure/Form1.Designer.cs
+++ Star Adventure/Star Adventure/Form1.Designer.cs
@@ -29,12 +29,323 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.falcon = new System.Windows.Forms.PictureBox();
+            this.asteroid = new System.Windows.Forms.PictureBox();
+            this.asteroid5 = new System.Windows.Forms.PictureBox();
+            this.asteroid2 = new System.Windows.Forms.PictureBox();
+            this.asteroid4 = new System.Windows.Forms.PictureBox();
+            this.asteroid3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.falcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(70, 130);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(166, 55);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(308, 105);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(207, 206);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(70, 265);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.White;
+            this.pictureBox7.Location = new System.Drawing.Point(352, 265);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.White;
+            this.pictureBox8.Location = new System.Drawing.Point(53, 473);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.White;
+            this.pictureBox9.Location = new System.Drawing.Point(12, 367);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.White;
+            this.pictureBox10.Location = new System.Drawing.Point(147, 334);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.White;
+            this.pictureBox11.Location = new System.Drawing.Point(255, 444);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BackColor = System.Drawing.Color.White;
+            this.pictureBox12.Location = new System.Drawing.Point(285, 355);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox12.TabIndex = 11;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.BackColor = System.Drawing.Color.White;
+            this.pictureBox13.Location = new System.Drawing.Point(401, 460);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox13.TabIndex = 12;
+            this.pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.BackColor = System.Drawing.Color.White;
+            this.pictureBox14.Location = new System.Drawing.Point(386, 12);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox14.TabIndex = 13;
+            this.pictureBox14.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // falcon
+            // 
+            this.falcon.Image = ((System.Drawing.Image)(resources.GetObject("falcon.Image")));
+            this.falcon.Location = new System.Drawing.Point(198, 427);
+            this.falcon.Name = "falcon";
+            this.falcon.Size = new System.Drawing.Size(67, 72);
+            this.falcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.falcon.TabIndex = 14;
+            this.falcon.TabStop = false;
+            // 
+            // asteroid
+            // 
+            this.asteroid.Image = ((System.Drawing.Image)(resources.GetObject("asteroid.Image")));
+            this.asteroid.Location = new System.Drawing.Point(29, 117);
+            this.asteroid.Name = "asteroid";
+            this.asteroid.Size = new System.Drawing.Size(51, 47);
+            this.asteroid.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid.TabIndex = 17;
+            this.asteroid.TabStop = false;
+            this.asteroid.Visible = false;
+            // 
+            // asteroid5
+            // 
+            this.asteroid5.Image = ((System.Drawing.Image)(resources.GetObject("asteroid5.Image")));
+            this.asteroid5.Location = new System.Drawing.Point(29, 12);
+            this.asteroid5.Name = "asteroid5";
+            this.asteroid5.Size = new System.Drawing.Size(51, 47);
+            this.asteroid5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid5.TabIndex = 18;
+            this.asteroid5.TabStop = false;
+            this.asteroid5.Visible = false;
+            // 
+            // asteroid2
+            // 
+            this.asteroid2.Image = ((System.Drawing.Image)(resources.GetObject("asteroid2.Image")));
+            this.asteroid2.Location = new System.Drawing.Point(29, 444);
+            this.asteroid2.Name = "asteroid2";
+            this.asteroid2.Size = new System.Drawing.Size(51, 47);
+            this.asteroid2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid2.TabIndex = 19;
+            this.asteroid2.TabStop = false;
+            this.asteroid2.Visible = false;
+            // 
+            // asteroid4
+            // 
+            this.asteroid4.Image = ((System.Drawing.Image)(resources.GetObject("asteroid4.Image")));
+            this.asteroid4.Location = new System.Drawing.Point(29, 334);
+            this.asteroid4.Name = "asteroid4";
+            this.asteroid4.Size = new System.Drawing.Size(51, 47);
+            this.asteroid4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid4.TabIndex = 20;
+            this.asteroid4.TabStop = false;
+            this.asteroid4.Visible = false;
+            // 
+            // asteroid3
+            // 
+            this.asteroid3.Image = ((System.Drawing.Image)(resources.GetObject("asteroid3.Image")));
+            this.asteroid3.Location = new System.Drawing.Point(29, 228);
+            this.asteroid3.Name = "asteroid3";
+            this.asteroid3.Size = new System.Drawing.Size(51, 47);
+            this.asteroid3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid3.TabIndex = 21;
+            this.asteroid3.TabStop = false;
+            this.asteroid3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(434, 511);
+            this.Controls.Add(this.asteroid3);
+            this.Controls.Add(this.asteroid4);
+            this.Controls.Add(this.asteroid2);
+            this.Controls.Add(this.asteroid5);
+            this.Controls.Add(this.asteroid);
+            this.Controls.Add(this.falcon);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Star Adventure";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.falcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox falcon;
+        private System.Windows.Forms.PictureBox asteroid;
+        private System.Windows.Forms.PictureBox asteroid5;
+        private System.Windows.Forms.PictureBox asteroid2;
+        private System.Windows.Forms.PictureBox asteroid4;
+        private System.Windows.Forms.PictureBox asteroid3;
     }
 }
 
--- Star Adventure/Star Adventure/Form1.cs
+++ Star Adventure/Star Adventure/Form1.cs
@@ -12,9 +12,153 @@ namespace Star_Adventure
 {
     public partial class Form1 : Form
     {
+        public List<PictureBox> Stars = new List<PictureBox>();
+        public List<PictureBox> Asteroids = new List<PictureBox>();
+        public int GameSpeed { get; set; }
+        public int Points { get; set; }
+        public Random Random { get; set; }
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.GameSpeed = 1;
+            this.Points = 0;
+            this.Random = new Random();
+            Stars.Add(pictureBox1);
+            Stars.Add(pictureBox2);
+            Stars.Add(pictureBox3);
+            Stars.Add(pictureBox4);
+            Stars.Add(pictureBox5);
+            Stars.Add(pictureBox6);
+            Stars.Add(pictureBox7);
+            Stars.Add(pictureBox8);
+            Stars.Add(pictureBox9);
+            Stars.Add(pictureBox10);
+            Stars.Add(pictureBox11);
+            Stars.Add(pictureBox12);
+            Stars.Add(pictureBox13);
+            Stars.Add(pictureBox14);
+            Asteroids.Add(asteroid);
+            Asteroids.Add(asteroid2);
+            Asteroids.Add(asteroid3);
+            Asteroids.Add(asteroid4);
+            Asteroids.Add(asteroid5);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveStars(GameSpeed);
+            moveAsteroids(GameSpeed);
+        }
+
+        void moveStars(int speed)
+        {
+            foreach (PictureBox star in Stars)
+            {
+                if (star.Top >= 550)
+                {
+                    star.Top = 0;
+                }
+                else
+                {
+                    star.Top += speed;
+                }
+            }
+
+        }
+
+        void moveAsteroids(int speed)
+        {
+            int end = Asteroids.Count;
+            for (int i = 0; i < end; i++)
+            {
+                if (Asteroids[i].Visible)
+                {
+                    if (Asteroids[i].Top >= 550)
+                    {
+                        Asteroids[i].Top = 0;
+                        Asteroids[i].Left = Random.Next(0, 381);
+                    }
+                    else
+                    {
+                        Asteroids[i].Top += speed;
+                    }
+                }
+                else
+                {
+                    Asteroids[i].Visible = true;
+                    Asteroids[i].Left = Random.Next(0, 381);
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (falcon.Left > 2)
+                {
+                    falcon.Left += -7;
+                }
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                if (falcon.Left < 360)
+                {
+                    falcon.Left += 7;
+                }
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                if (falcon.Left > 2)
+                {
+                    falcon.Left += -7;
+                }
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                if (falcon.Left < 360)
+                {
+                    falcon.Left += 7;
+                }
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (falcon.Top > 6)
+                {
+                    falcon.Top += -7;
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (falcon.Top < 434)
+                {
+                    falcon.Top += 7;
+                }
+                else
+                {
+                    falcon.Top = 437;
+                }
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                if (falcon.Top > 6)
+                {
+                    falcon.Top += -7;
+                }
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                if (falcon.Top < 434)
+                {
+                    falcon.Top += 7;
+                }
+                else
+                {
+                    falcon.Top = 437;
+                }
+            }
         }
     }
 }
